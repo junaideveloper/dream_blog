ff8af6be57e6fa2699617464950e70cd28fb3e6b
Junaid TT <junaidkodoor@gmail.com>
2021-03-08 23:48:16 +0400

    Triagram Similarity Search

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,9 +91,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
-        'USER':'*****',
-        'PASSWORD':'****',
-        'HOST':'localhost',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
         'PORT':'5433',
 
     }
